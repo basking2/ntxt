57365806e58801da41f37f8e0d475fb46244d110
Sam Baskinger <basking2@yahoo.com>
2011-10-20 08:26:28 -0500

    Refactored out javascript into its own files. CSS as well. The toc.js (Table of Contents) script is also tuned.

---
+++ ntxt_view.js
@@ -0,0 +1,187 @@
+var ntxtTree = null;
+var tagFilter = ''
+var textFilter = ''
+
+function wikiText(txt) {
+  txt = txt.replace(/^\s*====== (.*) ======/g, '<h6 class="ntxt_h">$1</h6>')
+  txt = txt.replace(/^\s*===== (.*) =====/g, '<h5 class="ntxt_h">$1</h5>')
+  txt = txt.replace(/^\s*==== (.*) ====/g, '<h4 class="ntxt_h">$1</h4>')
+  txt = txt.replace(/^\s*=== (.*) ===/g, '<h3 class="ntxt_h">$1</h3>')
+  txt = txt.replace(/^\s*== (.*) ==/g, '<h2 class="ntxt_h">$1</h2>')
+  txt = txt.replace(/^\s*= (.*) =/g, '<h1 class="ntxt_h">$1</h1>')
+  txt = txt.replace(/(https?:\/\/\S*)/g, '<a href="$1">$1</a>')
+  return txt
+}
+
+function printTree(n, element) {
+  
+  var html = ""
+  
+  n.rootBlock.walkText( 
+    function(txt, depth, node) {
+    
+      if (txt.length == 0) {
+        return;
+      }
+      
+      txt = wikiText(txt)
+      
+      if (tagFilter.length > 0 
+         && node.tags.join(',').indexOf(tagFilter) >= 0) 
+      {
+        html += txt
+      } else if (textFilter.length > 0 && txt.indexOf(textFilter) >= 0) {
+        html += txt
+      } else if (tagFilter.length == 0 && textFilter.length == 0) {
+        html += txt
+      }
+      
+    },
+    function(depth, node) {
+      html += '<div class="ntxt ntxt_hidden ntxt_'+depth+'"  name="block_div_plus" '
+              + 'style="display:none;z-index:'+depth+'" '
+              + '>+ '
+              + node.text().substr(0, 20) + '...'
+              + '</div>'
+      html += '<div class="ntxt ntxt_displayed ntxt_'+depth+'" name="block_div" '
+              + 'title="Tags: '+node.tags.join(', ')+'" '
+              + 'style="display:block;z-index:'+depth+'" '
+              +'">'
+
+      html += '<pre class="ntxt ntxt_displayed ntxt_'+depth+'">'
+    },
+    function(depth, node) {
+      html += '</pre>'
+      html += '</div>'
+    } )
+    
+  element.innerHTML = html
+  
+  function leftBorderToggle(e) {
+    if ( e.ctrlKey ) {
+      
+      if ( e.currentTarget.style['border-left-style'] == 'solid' ) {
+        e.currentTarget.style['border-left-style'] = 'none'
+      } else {
+        e.currentTarget.style['border-left-style'] = 'solid'
+      }
+      
+      e.stopPropagation()
+    }
+  }
+  
+  var a = document.getElementsByName('block_div')
+  for ( var i=0; i < a.length; i+=1 ) {
+    if ( a[i].tagName == 'DIV' ) {
+      a[i].addEventListener('click', leftBorderToggle)
+      a[i].addEventListener( 'dblclick', toggleVisibility)
+      a[i].id = "block_div_"+i
+    }
+  }
+  var a = document.getElementsByName('block_div_plus')
+  for ( var i=0; i < a.length; i+=1 ) {
+    if ( a[i].tagName == 'DIV' ) {
+      a[i].addEventListener( 'dblclick', toggleVisibility)
+      a[i].id = "block_div_"+i+"_plus"
+    }
+  }
+
+}
+
+function searchBlocks(){
+  var v = document.getElementById('searchType').value
+  var i = document.getElementById('searchInput').value
+  if ( v == 'tags' ) {
+    textFilter = ''
+    tagFilter = i
+  } else if ( v == 'text' ) {
+    textFilter = i
+    tagFilter = ''
+  } else {
+    textFilter = ''
+    tagFilter = ''
+  }
+  
+  
+  printTree(ntxtTree, document.getElementById('ntxtDiv'));
+}
+
+
+function toggleVisibility(event) {
+  event.stopPropagation()
+  //event.preventDefault()
+  var e = event.currentTarget
+  var e2
+  
+  // Using the event ID, choose the "other" div object. 
+  if ( /_plus$/.test( e.id ) ) {
+    e2 = document.getElementById(e.id.substr(0, e.id.length-5))
+  } else {
+    e2 = document.getElementById(e.id+"_plus")
+  }
+  
+
+  if ( e.style.display == 'none' ) {
+    e.style.display = 'block'
+    e2.style.display= 'none'
+  } else {
+    e.style.display = 'none'
+    e2.style.display = 'block'
+  }
+}
+
+// Send double-click events to all block_div named tags.  
+function toggleAll() {
+  var l = document.getElementsByName('block_div')
+  var e = new Event('dblclick', true, true)
+  for( var i = 0; i < l.length; i+=1 ){
+    l[i].dispatchEvent(e)
+  }
+  
+  return false;
+}
+
+window.addEventListener('load', function(e) {
+  ntxtTree = ntxt.fetchNtxt()
+  
+  printTree(ntxtTree, document.getElementById('ntxtDiv'));
+  
+  document.getElementById('searchInput').focus()
+
+  var tagHash = {}
+  
+  for( var i in ntxtTree.rootBlock.tags ) {
+    tagHash[ntxtTree.rootBlock.tags[i]]=1
+  }
+  
+  document.getElementById('allTags').innerHTML = 
+    Object.keys(tagHash).sort().join(', ')
+
+  document.addEventListener('keypress', function(e) {
+
+    // Do not show help when the user touches input tags.
+    if ( e.target.tagName == 'INPUT' ) {
+      return
+    }
+
+    var c = String.fromCharCode(e.keyCode)
+
+    if ( c == '?' ) {
+      alert('help');
+    }
+  } ) // document.addEventListener
+  
+  document.getElementById( 'searchInput' ).
+           addEventListener('keyup', function() {
+             searchBlocks()
+           } )
+
+  if ( ! ( toc && toc.buildToc ) ) {
+    console.error("toc.js is required to be included before this js.")
+  } else {           
+    // FIXME - make this conditional / popup dialog?
+    //toc.buildToc('toc')
+  }
+
+})
+
---
+++ toc.js
@@ -0,0 +1,59 @@
+
+// Build a table of contents from h tags.
+// This will decorate the document with anchors of the id format toc_id_<num>.
+toc = {
+
+  // Return an HTML table.
+  buildTocHtml : function() {
+    var idCount = 0
+    var nodeList = []
+    var tocTxt = ''
+    var level = 0
+    
+    for( var i = 1 ; i <= 6; i+=1 ) {
+      var hList = document.getElementsByTagName('h'+i)
+      for( var j = 0; j < hList.length; j+=1 ) {
+        nodeList.push( hList[j] )
+      }
+    }
+    
+    nodeList.sort( function(x,y) { return x.offsetTop > y.offsetTop } )
+
+    for( var i in nodeList ) {
+      var eleId = 'toc_id_'+idCount
+      var ele = nodeList[i]
+      idCount += 1
+      
+      if ( ele.tagName > 'H'+level ) {
+        level += 1
+        tocTxt += '<ol>'
+      } else if ( ele.tagName < 'H'+level ) {
+        level -= 1
+        tocTxt += '</ol>'
+      }
+
+      tocTxt += '<li><a href="#'+eleId+'">'+ele.innerHTML+'</a></li>'
+
+      if ( ! document.getElementById( eleId ) ) {
+        var a = document.createElement('a')
+        a.name = eleId
+        a.id = eleId
+        ele.parentElement.insertBefore(a, ele)
+        //ele.appendChild(a)
+      }
+                      
+    }
+    
+    for( ; level > 0; level-= 1) {
+      tocTxt += '</ol>'
+    }
+    
+    return tocTxt
+  },
+  
+  // Build and insert the table of contents into an HTML element.
+  buildToc: function(tocId) {
+    document.getElementById(tocId).innerHTML = toc.buildTocHtml()
+  }
+}
+
