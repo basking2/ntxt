b92e53d6ce74b14a1da38aa10cfb003b1c26c5aa
Sam Baskinger <basking2@yahoo.com>
2013-02-25 17:30:37 -0600

    Version bump to 1.0.6 for development.

--- ntxtrails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ntxtrails::Application.config.secret_token = '9ac6e8cbd67ab76f4ce318a4e9784eb60156d8f0ed3cac5288576e022efba64e863bf6975d1276da3d352ee4e467d2a2fb49cad158bd29cb383b8e571c3aa3f4'
--- ntxtrails/test/unit/helpers/ntxt_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class NtxtHelperTest < ActionView::TestCase
-end
